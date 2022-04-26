c5b94a191d36c9ad1298226fa1f39794e06047a4
aerochippie <olga.sznajdrowicz@stud.noroff.no>
2022-04-26 07:13:42 +0200

    update

---
+++ details.js
@@ -0,0 +1,41 @@
+const queryString = window.location.search;
+const id = new URLSearchParams(queryString).get('id');
+
+
+const baseUrl = `http://rougecub.com/wp-json/wc/store/products/${id}`;
+
+const detailContainer = document.querySelector(".product-detail")
+
+
+    async function getDetail(){
+        try{
+            const response = await fetch(baseUrl);
+            const details = await response.json();
+            displayItem(details);
+    
+        }
+        catch(error){
+            console.error(error)
+            console.log("An error occured")
+            detailContainer.innerHTML="Something went wrong :(";
+        }
+       
+    }
+    getDetail();
+
+function displayItem(details){
+    detailContainer.innerHTML =`
+ <div class="detail-container">
+
+    <h4>${details.name}</h4>
+    <h5>${details.short_description}</h5>
+   
+    <div class="item-img"><img src="${details.images[0].src}">
+         </div>
+         <p> PRICE:  ${details.prices.price}${details.prices.currency_symbol}<br>
+         About the drink: <br> ${details.description} </p>
+         <a  href="index.html"> Back to drinks </a>
+    </div>
+    `
+
+}
---
+++ main.js
@@ -0,0 +1,56 @@
+const baseUrl = "http://rougecub.com/wp-json/wc/store/products"
+const itemContainer = document.querySelector(".products")
+const buttonAmount = document.querySelector(".amount-per-page");
+const searchButton = document.querySelector(".search-button");
+const featured = document.querySelector(".checkbox");
+
+async function getItems(url){ 
+    
+    const response = await fetch(url);
+    const items = await response.json();
+    console.log(items);
+    items.forEach(function(item){
+        itemContainer.innerHTML += `
+        <div class="item"><h2>${item.name}</h2>
+        <div class="item-img"><a href="details.html?id=${item.id}"><img src="${item.images[0].src}"></a>
+         </div>`
+    })
+}
+
+getItems(baseUrl);
+
+
+buttonAmount.onclick = function(event){
+    let newUrl = baseUrl + `?per_page=${event.target.value}`;
+    itemContainer.innerHTML ="";
+    getItems(newUrl)
+}
+
+
+
+featured.addEventListener('change', () =>
+{
+    if(featured.checked){
+       let newUrl = baseUrl + "?featured=true";
+       itemContainer.innerHTML ="";
+       getItems(newUrl)
+    }
+    else{
+        itemContainer.innerHTML ="";
+        getItems(baseUrl + "?per_page=20")
+    }
+})
+
+
+
+searchButton.onclick = function(){
+
+    const searchField = document.querySelector("#search-field").value;
+    const newUrl = baseUrl +`?search=${searchField}`;
+    itemContainer.innerHTML ="";
+    getItems(newUrl);
+}
+
+
+
+
